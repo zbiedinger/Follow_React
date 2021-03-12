784afc9cac4a97d4d24a01d64999b351f0d40927
Zackery Biedinger <zbiedinger@mac.com>
2021-03-12 15:02:07 -0500

    updated Counter class with an img and count in state and added formatting to render function

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,9 +1,31 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Counter extends Component {
-    render() { 
-        return <h1>Hello World</h1>;
-    }
+  state = {
+    count: 10,
+    imageUrl: "https://picsum.photos/200",
+  };
+
+  render() {
+    return (
+      <React.Fragment>
+        <img src={this.state.imageUrl} alt="" />
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button className="btn btn-secondary btn-sm">Increment</button>
+      </React.Fragment>
+    );
+  }
+
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
+  }
+
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? "Zero" : count;
+  }
 }
- 
+
 export default Counter;
