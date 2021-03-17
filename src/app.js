63a8b507224aaca00221b9994e67a4e245a3ef83
Zackery Biedinger <zbiedinger@mac.com>
2021-03-17 14:41:14 -0400

    Added a navbar with the number of current items

--- src/app.js
+++ src/app.js
@@ -1,15 +1,54 @@
 import React, { Component } from 'react';
 import Counters from './components/counters';
 import Movies from './components/movies';
+import NavBar from './components/navbar';
 
 
 class App extends Component {
+    state = {
+        counters: [
+          { id: 1, value: 4 },
+          { id: 2, value: 0 },
+          { id: 3, value: 0 },
+          { id: 4, value: 0 },
+        ],
+      };
+    
+      handleIncrement = (counter) => {
+        const counters = [...this.state.counters];
+        const index = counters.indexOf(counter);
+        counters[index] = { ...counter };
+        counters[index].value++;
+        this.setState({ counters });
+      };
+    
+      handleReset = () => {
+        const counters = this.state.counters.map((c) => {
+          c.value = 0;
+          return c;
+        });
+        this.setState({ counters: counters });
+      };
+    
+      handleDelete = (counterId) => {
+        const counters = this.state.counters.filter((c) => c.id !== counterId);
+        this.setState({ counters });
+      };
+
     render() { 
         return ( 
+            <React.Fragment>
+            <NavBar totalCounters={this.state.counters.filter(c => c.value > 0).length}/>
             <main className="container">
-                <Counters/>
+                <Counters
+                counters={this.state.counters}
+                onReset={this.handleReset}
+                onIncrement={this.handleIncrement}
+                onDelete={this.handleDelete}
+                />
                 <Movies/>
             </main>
+            </React.Fragment>
          );
     }
 }
