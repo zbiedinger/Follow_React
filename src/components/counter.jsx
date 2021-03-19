ff5f49cbb7d4124d5f3cef81f08c3dd485d2f6b1
Zackery Biedinger <zbiedinger@mac.com>
2021-03-19 11:20:34 -0400

    The decrement button now gets disabled if counter is zero

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -32,6 +32,7 @@ class Counter extends Component {
           <button
             onClick={() => this.props.onDecrement(this.props.counter)}
             className="btn btn-secondary btn-sm m-2"
+            disabled={this.props.counter.value === 0 ? "disabled" : ""}
           >
             -
           </button>
